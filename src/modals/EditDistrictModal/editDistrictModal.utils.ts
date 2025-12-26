5e38598a58539f3b7a76eecaf80c2e87527b6e74
Ilya Pogorelov <Ilya.Pogorelov@contractor.cengage.com>
2025-12-27 00:38:15 +0200

    ditto

--- src/modals/EditDistrictModal/editDistrictModal.utils.ts
+++ src/modals/EditDistrictModal/editDistrictModal.utils.ts
@@ -5,17 +5,29 @@ import { toNumber, toString } from "../../utilities/utilities.ts";
 import type { EditDistrictData } from "./editDistrictModal.types.ts";
 
 export const toData = (district: District): EditDistrictData => ({
-  pos: district.position.map((n) => toString(n)) as [string, string, string],
-  min: district.transMin.map((n) => toString(n)) as [string, string, string],
-  max: district.transMax.map((n) => toString(n)) as [string, string, string],
+  pos: district.position.map((n) => toString(n)).slice(0, 3) as [
+    string,
+    string,
+    string,
+  ],
+  min: district.transMin.map((n) => toString(n)).slice(0, 3) as [
+    string,
+    string,
+    string,
+  ],
+  max: district.transMax.map((n) => toString(n)).slice(0, 3) as [
+    string,
+    string,
+    string,
+  ],
   cubeSize: toString(district.cubeSize),
 });
 
 export const fromData = (
   data: EditDistrictData,
 ): Pick<District, "position" | "cubeSize" | "transMin" | "transMax"> => ({
-  position: data.pos.map((n) => toNumber(n)) as THREE.Vector3Tuple,
-  transMin: data.min.map((n) => toNumber(n)) as THREE.Vector4Tuple,
-  transMax: data.max.map((n) => toNumber(n)) as THREE.Vector4Tuple,
+  position: data.pos.map((n) => toNumber(n)).slice(0, 3) as THREE.Vector3Tuple,
+  transMin: data.min.map((n) => toNumber(n)).slice(0, 3) as THREE.Vector4Tuple,
+  transMax: data.max.map((n) => toNumber(n)).slice(0, 3) as THREE.Vector4Tuple,
   cubeSize: toNumber(data.cubeSize),
 });
