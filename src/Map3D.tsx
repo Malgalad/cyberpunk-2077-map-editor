75a05d74faf822def77308bb65a8fe5ac558e07d
Ilya Pogorelov <Ilya.Pogorelov@contractor.cengage.com>
2026-03-14 23:27:05 +0200

    WIP

--- src/Map3D.tsx
+++ src/Map3D.tsx
@@ -1,4 +1,4 @@
-import { MapControls } from "@react-three/drei";
+import { MapControls, OrthographicCamera } from "@react-three/drei";
 import { Canvas, useLoader, useThree } from "@react-three/fiber";
 import * as React from "react";
 import * as THREE from "three/webgpu";
@@ -8,14 +8,27 @@ import { KNOWN_MESHES } from "./constants.ts";
 import { useAppSelector, useAppStore } from "./hooks/hooks.ts";
 import { STATIC_ASSETS } from "./map3d/constants.ts";
 import { frustumSize } from "./map3d/map3d.base.ts";
-import { staticMaterial } from "./map3d/materials.ts";
+import {
+  additionsMaterial,
+  buildingsMaterial,
+  staticMaterial,
+} from "./map3d/materials.ts";
 import { materialsMap } from "./map3d/setupTerrain.ts";
 import mapData from "./mapData.min.json";
 import { DistrictSelectors } from "./store/district.ts";
 import { NodesSelectors } from "./store/nodesV2.ts";
 import { OptionsSelectors } from "./store/options.ts";
-import type { District, DistrictWithTransforms } from "./types/types.ts";
-import { getFinalDistrictTransformsFromNodes } from "./utilities/district.ts";
+import type {
+  District,
+  InstancedMeshTransforms,
+  TreeNode,
+} from "./types/types.ts";
+import {
+  getFinalDistrictTransformsFromNodes,
+  immutableDistrictTransforms,
+} from "./utilities/district.ts";
+import { getTransformsFromSubtree } from "./utilities/getTransformsFromSubtree.ts";
+import { invariant, partition } from "./utilities/utilities.ts";
 
 // region static
 function useDracoLoader(url: string[]) {
@@ -103,10 +116,178 @@ const emptyArray: any[] = [];
 const boxScale = [1, 1, 1] as const;
 const getCount = (length: number) =>
   Math.max(1000, Math.ceil(length / 1000) * 1000);
+
+function useUpsertInstances(
+  meshRef: React.RefObject<THREE.InstancedMesh | null>,
+  transforms: InstancedMeshTransforms[],
+  district?: District,
+) {
+  const three = useThree();
+
+  React.useEffect(() => {
+    const mesh = meshRef.current;
+
+    if (!mesh || !district) return;
+
+    const position = new THREE.Vector3().fromArray(district.position);
+    const transformMin = new THREE.Vector4().fromArray(district.transMin);
+
+    mesh.position.set(
+      position.x + transformMin.x,
+      position.z + transformMin.z,
+      -position.y - transformMin.y,
+    );
+
+    const matrix = new THREE.Matrix4();
+    let needsUpdate = false;
+
+    for (let index = 0; index < transforms.length; index++) {
+      const instance = transforms[index];
+
+      // if (instance === mesh.userData.instances[index]) continue;
+
+      const position = new THREE.Vector3(
+        instance.position.x,
+        instance.position.z,
+        -instance.position.y,
+      );
+      const rotation = new THREE.Quaternion(
+        instance.orientation.x,
+        instance.orientation.z,
+        -instance.orientation.y,
+        instance.orientation.w,
+      );
+      const scale = new THREE.Vector3(
+        instance.scale.x,
+        instance.scale.z,
+        instance.scale.y,
+      );
+
+      needsUpdate = true;
+      matrix.compose(position, rotation, scale);
+      mesh.setMatrixAt(index, matrix);
+      // if (color) mesh.setColorAt(index, color);
+    }
+
+    if (needsUpdate) mesh.instanceMatrix.needsUpdate = true;
+    if (needsUpdate) mesh.computeBoundingSphere();
+    if (needsUpdate) three.invalidate();
+  }, [district, transforms, meshRef, three]);
+}
+
+function StaticInstancedMesh(props: { district: District; visible?: boolean }) {
+  const { district, ...rest } = props;
+  const store = useAppStore();
+  const meshRef = React.useRef<THREE.InstancedMesh | null>(null);
+  const transforms = React.useMemo(() => {
+    if (!district) return emptyArray;
+
+    const state = store.getState();
+    const nodes = NodesSelectors.getNodes(state);
+    const tree = NodesSelectors.getNodesTree(state);
+
+    return getFinalDistrictTransformsFromNodes(district, nodes, tree);
+  }, [district, store]);
+
+  useUpsertInstances(meshRef, transforms, district);
+
+  return (
+    <instancedMesh
+      ref={meshRef}
+      args={[undefined, undefined, getCount(transforms.length)]}
+      material={staticMaterial}
+      {...rest}
+    >
+      <boxGeometry args={boxScale} />
+    </instancedMesh>
+  );
+}
+
+function CurrentInstancedMesh(props: {
+  district?: District;
+  visible?: boolean;
+}) {
+  const { district, ...rest } = props;
+  const store = useAppStore();
+  const nodes = useAppSelector(NodesSelectors.getNodes);
+  const tree = useAppSelector(NodesSelectors.getNodesTree);
+  const baselineMesh = React.useRef<THREE.InstancedMesh | null>(null);
+  const additionsMesh = React.useRef<THREE.InstancedMesh | null>(null);
+  const baselineTransforms = React.useMemo(() => {
+    if (!district) return emptyArray;
+
+    const state = store.getState();
+    const nodes = NodesSelectors.getNodes(state);
+    const index = NodesSelectors.getNodesIndex(state);
+    const root = index[district.name];
+
+    const baseTransforms = immutableDistrictTransforms.get(district.name) ?? [];
+
+    if (root) {
+      const { update: updates = [], delete: deletions = [] } = partition(
+        root.descendantIds,
+        (id) => nodes[id].tag,
+      );
+      const updateIndexes = new Set(
+        updates.map((id) => nodes[id].indexInDistrict),
+      );
+      const deletionIndexes = new Set(
+        deletions.map((id) => nodes[id].indexInDistrict),
+      );
+
+      return baseTransforms.map((instance, index) => {
+        if (updateIndexes.has(index) || deletionIndexes.has(index)) {
+          return { ...instance, scale: { x: 0, y: 0, z: 0, w: 0 } };
+        }
+
+        return instance;
+      });
+    } else {
+      return baseTransforms;
+    }
+  }, [district, store]);
+
+  const additions = React.useMemo<TreeNode[]>(() => {
+    if (!district || !tree[district.name]) return emptyArray;
+    const root = tree[district.name];
+    invariant(root.type === "district", "Unexpected tree node type");
+    return root.create;
+  }, [district, tree]);
+  const additionsTransforms = React.useMemo(
+    () =>
+      district
+        ? getTransformsFromSubtree(district, nodes, additions)
+        : emptyArray,
+    [district, nodes, additions],
+  );
+
+  useUpsertInstances(baselineMesh, baselineTransforms, district);
+  useUpsertInstances(additionsMesh, additionsTransforms, district);
+
+  return (
+    <>
+      <instancedMesh
+        ref={baselineMesh}
+        args={[undefined, undefined, getCount(baselineTransforms.length)]}
+        material={buildingsMaterial}
+        {...rest}
+      >
+        <boxGeometry args={boxScale} />
+      </instancedMesh>
+      <instancedMesh
+        ref={additionsMesh}
+        args={[undefined, undefined, getCount(additionsTransforms.length)]}
+        material={additionsMaterial}
+        {...rest}
+      >
+        <boxGeometry args={boxScale} />
+      </instancedMesh>
+    </>
+  );
+}
+
 function Meshes() {
   const three = useThree();
-  const refs = React.useRef<Record<string, THREE.InstancedMesh | null>>({});
-  const store = useAppStore();
   const currentDistrict = useAppSelector(DistrictSelectors.getDistrict);
   const allDistricts = useAppSelector(DistrictSelectors.getAllDistricts);
   const districtView = useAppSelector(OptionsSelectors.getDistrictView);
@@ -123,115 +304,64 @@ function Meshes() {
     };
     const visibleDistricts = districtsVisibilityMap[districtView];
 
-    if (!currentDistrict) return visibleDistricts;
+    if (!currentDistrict)
+      return visibleDistricts.map((district) => district.name);
 
-    return visibleDistricts.filter(
-      (district) => district.name !== currentDistrict.name,
-    );
+    return visibleDistricts
+      .filter((district) => district.name !== currentDistrict.name)
+      .map((district) => district.name);
   }, [currentDistrict, allDistricts, visibleDistrictNames, districtView]);
-  const districtsWithTransforms = React.useMemo<
-    DistrictWithTransforms[]
-  >(() => {
-    const state = store.getState();
-    const nodes = NodesSelectors.getNodes(state);
-    const tree = NodesSelectors.getNodesTree(state);
-
-    return nonCurrentDistricts.map((district) => ({
-      district,
-      transforms: getFinalDistrictTransformsFromNodes(district, nodes, tree),
-    }));
-  }, [nonCurrentDistricts, store]);
 
   React.useEffect(() => {
-    for (const { district, transforms } of districtsWithTransforms) {
-      const mesh = refs.current[district.name];
-      if (!mesh) continue;
-
-      const position = new THREE.Vector3().fromArray(district.position);
-      const transformMin = new THREE.Vector4().fromArray(district.transMin);
-
-      mesh.position.set(
-        position.x + transformMin.x,
-        position.z + transformMin.z,
-        -position.y - transformMin.y,
-      );
-      const temp = new THREE.Matrix4();
-      for (let i = 0; i < transforms.length; i += 1) {
-        const transform = transforms[i];
-        const position = new THREE.Vector3(
-          transform.position.x,
-          transform.position.z,
-          -transform.position.y,
-        );
-        const rotation = new THREE.Quaternion(
-          transform.orientation.x,
-          transform.orientation.z,
-          -transform.orientation.y,
-          transform.orientation.w,
-        );
-        const scale = new THREE.Vector3(
-          transform.scale.x,
-          transform.scale.z,
-          transform.scale.y,
-        );
-        temp.compose(position, rotation, scale);
-        mesh.setMatrixAt(i, temp);
-      }
-      mesh.instanceMatrix.needsUpdate = true;
-    }
     three.invalidate();
-  }, [districtsWithTransforms, three]);
+  }, [three, nonCurrentDistricts]);
 
   return (
     <>
-      {districtsWithTransforms.map(({ district, transforms }) => (
-        <instancedMesh
-          ref={(node) => (refs.current[district.name] = node)}
-          args={[undefined, undefined, getCount(transforms.length)]}
+      {allDistricts.map((district) => (
+        <StaticInstancedMesh
           key={district.name}
-          material={staticMaterial}
-          position={district.position}
-        >
-          <boxGeometry args={boxScale} />
-        </instancedMesh>
+          district={district}
+          visible={
+            nonCurrentDistricts.includes(district.name) &&
+            district.name !== currentDistrict?.name
+          }
+        />
       ))}
+      <CurrentInstancedMesh
+        district={currentDistrict}
+        visible={!!currentDistrict}
+      />
     </>
   );
 }
 
-function Map3D() {
-  const canvas = React.useRef<HTMLCanvasElement | null>(null);
-  const camera = React.useMemo(
-    () =>
-      new THREE.OrthographicCamera(
-        frustumSize / -2,
-        frustumSize / 2,
-        frustumSize / 2,
-        frustumSize / -2,
-        0.1,
-        20_000,
-      ),
-    [],
-  );
+function useSetupCamera(canvas: React.RefObject<HTMLCanvasElement | null>) {
+  const cameraRef = React.useRef<THREE.OrthographicCamera | null>(null);
 
   React.useEffect(() => {
-    if (!canvas.current) return;
+    const camera = cameraRef.current;
+    if (!canvas.current || !camera) return;
     const aspect = canvas.current.clientWidth / canvas.current.clientHeight;
     camera.left = (frustumSize * aspect) / -2;
     camera.right = (frustumSize * aspect) / 2;
     camera.top = frustumSize / 2;
     camera.bottom = frustumSize / -2;
-    camera.position.set(0, 3000, 0);
     camera.lookAt(0, 0, 0);
-    camera.zoom = 1;
     camera.updateProjectionMatrix();
-  }, [camera]);
+  }, [cameraRef, canvas]);
+
+  return cameraRef;
+}
+
+function Map3D() {
+  const canvas = React.useRef<HTMLCanvasElement | null>(null);
+  const cameraRef = useSetupCamera(canvas);
 
   return (
     <Canvas
       ref={canvas}
       orthographic
-      camera={camera}
       gl={async (props) => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const renderer = new THREE.WebGPURenderer(props as any);
@@ -240,11 +370,19 @@ function Map3D() {
       }}
       frameloop="demand"
     >
+      <OrthographicCamera
+        makeDefault
+        position={[0, 3000, 0]}
+        near={0.1}
+        far={5_000}
+        ref={cameraRef}
+      />
       <MapControls
         zoomToCursor
-        maxDistance={10_000}
+        maxDistance={5_000}
         minDistance={1}
         maxPolarAngle={Math.PI / 2}
+        enableDamping={false}
       />
       <Lights />
       <StaticAssets />
