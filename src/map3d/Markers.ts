c9bea3cb0460bec72909534dfd25d803dd40b7dc
Ilya Pogorelov <Ilya.Pogorelov@contractor.cengage.com>
2026-05-01 15:24:37 +0300

    Update vite plugins

--- src/map3d/Markers.ts
+++ src/map3d/Markers.ts
@@ -35,7 +35,6 @@ class Markers extends THREE.Group<EventMap> {
   );
   private markerNodes: MapNode[] = [];
   name = "Markers";
-  renderOrder = 1000;
 
   constructor(store: AppStore, camera: THREE.OrthographicCamera) {
     super();
